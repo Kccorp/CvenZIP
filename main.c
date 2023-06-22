770a73d9b96e4bf5dd85881eac6b55adac10a713
m3kaniml <m3kaniml@gmail.com>
2023-06-22 21:09:19 +0200

    full merge to main

--- main.c
+++ main.c
@@ -52,74 +52,31 @@ void extract_file(struct zip *zip, char *filename) { //fonction qui va extraire 
     int len;
     int err;
 
-    printf("extracting file '%s'...\n", filename);
-    file = zip_fopen(zip, filename, 0); //on ouvre le fichier zip et on le stocke dans la structure file
-    if (file == NULL) {
-        printf("can't open file '%s': %s\n", filename, zip_strerror(zip));
+    zip = zip_open(filename, 0, &err); //ouvre le zip et stocke le resultat dans la structure zip
+    if (zip == NULL) {
+        printf("can't open zip archive '%s': %s\n", filename, zip_strerror(zip)); // a modifier afin que si une erreur précise , on puisse l'afficher
         exit(1);
     }
 
-    FILE *outfile = fopen(filename, "w"); //on crée un fichier de nom filename
-    if (outfile == NULL) {
-        printf("can't open file '%s': %s\n", filename, strerror(errno));
+    return zip; //on retourne la structure zip afin de l'utiliser dans le main
+}
+
+void close_zip(struct zip *zip) {
+    if (zip_close(zip) < 0) { //on ferme le fichier zip avec la fonction zip_close qui prend en parametre la structure zip et retourne -1 si il y a une erreur
+        printf("can't close zip archive '%s': %s\n", "test.zip", zip_strerror(zip));
         exit(1);
     }
-
-    while ((len = zip_fread(file, buffer, sizeof(buffer))) > 0) { //tant qu'on peut lire dans le fichier zip
-        fwrite(buffer, 1, len, outfile); //on stocke da,s le fichier filename
-    }
-
-    if (len < 0) {
-        printf("can't read file '%s': %s\n", filename, zip_file_strerror(file));
-        exit(1);
-    }
-
-    if (zip_fclose(file) < 0) { //on ferme le fichier zip
-        printf("can't close file '%s': %s\n", filename, zip_file_strerror(file));
-        exit(1);
-    }
-
-    fclose(outfile); //on ferme le fichier filename
-    printf("file '%s' extracted\n", filename);
+    printf("closing zip archive...\n");
 }
 
 int main() {
-    // Demande le nom du fichier à décompresser
-    char filename[MAX_FILENAME_LENGTH];
-    printf("Entrez le nom du fichier à décompresser : ");
-    fgets(filename, MAX_FILENAME_LENGTH, stdin);
-    filename[strcspn(filename, "\n")] = '\0';  // Supprime le caractère de nouvelle ligne
+    struct zip *zip;
+    int num_files;
 
-    // Demande le mot de passe
-    char password[MAX_PASSWORD_LENGTH];
-    printf("Entrez le mot de passe du fichier (laissez vide s'il n'y en a pas) : ");
-    fgets(password, MAX_PASSWORD_LENGTH, stdin);
-    password[strcspn(password, "\n")] = '\0';  // Supprime le caractère de nouvelle ligne
+    zip = open_zip("test.zip");
 
-    // Ouvre le fichier zip
-    int error;
-    zip_t *zip = zip_open(filename, 0, &error);
-    if (zip == NULL) {
-        printf("Impossible d'ouvrir le fichier zip : %s\n", zip_strerror(error));
-        return 1;
-    }
-
-    // Vérifie si un mot de passe est nécessaire
-    if (zip_set_default_password(zip, password) < 0) {
-        printf("Mot de passe incorrect ou nécessaire pour extraire le fichier.\n");
-        zip_close(zip);
-        return 1;
-    }
-
-    // Obtient le nombre d'entrées dans le fichier zip
-    int num_entries = zip_get_num_entries(zip, 0);
-    printf("Nombre d'entrées dans le fichier zip : %d\n", num_entries);
-
-    // Parcourt les entrées et extrait les fichiers
-    for (int i = 0; i < num_entries; i++) {
-        // Obtient l'information de l'entrée
-        const char *name = zip_get_name(zip, i, 0);
-        printf("Extraction de : %s\n", name);
+    num_files = zip_get_num_files(zip); // pour tester si ça ouvrait bien , print le nombre de fichier
+    printf("number of files in zip: %d\n", num_files);
 
         // Ouvre le fichier dans le fichier zip
         zip_file_t *file = zip_fopen_index(zip, i, 0);
